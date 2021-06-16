e6f341cc876b5a039391399dfece570c8bd5772a
Team <super_day3223>
2021-06-16 19:31:26 +0200

    deleted old vars

--- KTDVS/KTDVS/main.cpp
+++ KTDVS/KTDVS/main.cpp
@@ -56,36 +56,19 @@ int main(int argc, char *argv[])
         //}
         //else if(arg==QString("test")){
             qDebug() << "Testfall wurde aufgerufen";
-            QSqlQuery   OrgM,OrgK,
+            QSqlQuery   HOrg,OrgM,OrgK,
                         t1,t2,t3,t4,t5,t6,t7,t8,t9,t10;
-            QSqlQuery   HOrg_1, HOrg_2, HOrg_3,
-                        OrgM_1, OrgM_2, OrgM_3,
-                        OrgK_1, OrgK_2, OrgK_3,
-                        td1_1, td1_2, td1_3,
-                        td2_1, td2_2, td2_3,
-                        td3_1, td3_2, td3_3,
-                        td4_1, td4_2, td4_3,
-                        td5_1, td5_2, td5_3,
-                        td6_1, td6_2, td6_3,
-                        td7_1, td7_2, td7_3,
-                        td8_1, td8_2, td8_3,
-                        td9_1, td9_2, td9_3,
-                        td10_1, td10_2, td10_3;
-            QSqlQuery   HOrg_tel_1, HOrg_tel_2, HOrg_tel_3,
-                        OrgM_tel_1, OrgM_tel_2, OrgM_tel_3,
-                        OrgK_tel_1, OrgK_tel_2, OrgK_tel_3,
-                        tel1_1, tel1_2, tel1_3,
-                        tel2_1, tel2_2,
-                        tel3_1, tel3_2,
-                        tel4_1, tel4_2,
-                        tel5_1, tel5_2, tel5_3,
-                        tel6_1, tel6_2, tel6_3,
-                        tel7_1, tel7_2, tel7_3,
-                        tel8_1, tel8_2, tel8_3,
-                        tel9_1, tel9_2, tel9_3,
-                        tel10_1, tel10_2, tel10_3;
+            QSqlQuery   HOrg_1, OrgM_1, OrgK_1,
+                        td1_1, td2_1, td3_1,
+                        td4_1, td5_1, td6_1,
+                        td7_1, td8_1, td9_1,
+                        td10_1;
+            QSqlQuery   HOrg_tel_1, OrgM_tel_1, OrgK_tel_1,
+                        tel1_1, tel2_1, tel3_1,
+                        tel4_1, tel5_1, tel6_1,
+                        tel7_1, tel8_1, tel9_1,
+                        tel10_1;
             //Hauptorganisator Roland Dietrich mit Kennwort "RD"
-            QSqlQuery HOrg;
             HOrg.prepare("INSERT INTO Teilnehmer (passwort,isHauptorganisator,versuch,isSystempasswort) VALUES ('RD', true, 0, true)");
 
 
